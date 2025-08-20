54294fcfadb3ffa1c3fbef298f9189b297c635ee
mapurite <dfpt1275@gmail.com>
2025-08-19 23:42:50 -0300

    Update api_usd.py

--- api_usd.py
+++ api_usd.py
@@ -1,7 +1,34 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
 import requests
+
 
 def obtener_api():
     r = requests.get("https://api.exchangerate.host/latest?base=USD")
-    print(r.json())
+    data = r.json()
+    print("Cotizaciones del USD:")
+    print(data)
 
-PythonOperator(task_id="cotizacion_usd", python_callable=obtener_api)
+
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1),
+}
+
+with DAG(
+    dag_id="cotizacion_api_dag",
+    default_args=default_args,
+    description="Ejemplo DAG que consulta una API",
+    schedule_interval="@daily",    # corre todos los días
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    tags=["api", "ejemplo"],
+) as dag:
+
+    consulta_api = PythonOperator(
+        task_id="cotizacion_usd",
+        python_callable=obtener_api,
+    )
+
